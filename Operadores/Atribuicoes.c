2c5643b0786190fe66f05967a9dc62aeda6aac68
airtnsnts <airtnsnts@gmail.com>
2026-02-28 23:06:49 -0300

    Pasta de Atribuições

---
+++ Operadores/Atribuicoes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    /*Atribuição Simples (=)
+      Atribuição Com Soma (+=)
+      Atribuição Com Subtração (-=)
+      Atribuição Com Multiplicação (*=)
+      Atribuição Com Divisão (/=)
+      */
+    int n1, n2, resultado;
+
+    resultado = 10;
+    printf("Resultado: %d\n", resultado);
+
+    resultado += 20;
+    printf("Resultado: %d\n", resultado);
+
+    resultado -= 6;
+    printf("Resultado: %d\n", resultado);
+
+    resultado *= 10;
+    printf("Resultado: %d\n", resultado);
+
+    resultado /= 4;
+    printf("Resultado: %d\n", resultado);
+
+
+}
