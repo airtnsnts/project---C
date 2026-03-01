ee1828e65256ce3c0e8a6038ec298e7f8987266d
airtnsnts <airtnsnts@gmail.com>
2026-02-28 23:54:57 -0300

    Encremento de Decremento

---
+++ Operadores/incrementar.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    /* 
+     Icrementar (++)
+     Pré- Icrementar ++a
+     Pós- Icrementar a++
+     Decrementar (--)
+     Pré- Decrementar --a
+     Pós- Decrementar a--
+    */
+    int n1 = 1, resultado;
+    printf("Antes do incremento: %d\n", n1);
+    //n1++;
+    //n1 += 1;
+
+    //printf("Após do incremento: %d\n", n1);
+    //n1--;
+    //n1 -= 1;
+    //printf("Após do decremento: %d\n", n1);
+    resultado = n1++;
+    printf("Após-incremeto - Numero 1: %d - Resultado: %d\n", n1, resultado);
+
+    resultado = ++n1;
+    printf("Pré-incremeto - Numero 1: %d - Resultado: %d\n", n1, resultado);
+
+    resultado = n1--;
+    printf("Após-decremeto - Numero 1: %d - Resultado: %d\n", n1, resultado);
+
+    resultado = --n1;
+    printf("Após-decremeto - Numero 1: %d - Resultado: %d\n", n1, resultado);
+
+    
+}
