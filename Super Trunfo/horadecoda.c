71b73f9de910321bb750b1ec0712b0c7d82c28a5
airtnsnts <airtnsnts@gmail.com>
2026-03-06 17:45:16 -0300

    Calculando média

--- Super Trunfo/horadecoda.c
+++ Super Trunfo/horadecoda.c
@@ -2,6 +2,39 @@
 
 int main() {
 
- 
+    char aluno[50] ;
+    int matricul;
+    float mat, port, hist, art, edf;
+    float media;
+
+printf("Colégio Nestor Gomes de Araujo\n");
+printf("Média dos aluno no Primeiro\n");
+printf("Nome:");
+scanf("%s", aluno);
+
+printf("Matrícula:");
+scanf(" %d", &matricul);
+
+printf("Digite as notas do aluno:\n");
+printf("Matematica:");
+scanf("%f", &mat);
+
+printf("Português:");
+scanf("%f", &port);
+
+printf("História:");
+scanf("%f", &hist);
+
+printf("Artes:");
+scanf("%f", &art);
+
+printf("Educação Fisica:");
+scanf("%f", &edf);
+
+media = (mat + port + hist + art + edf) / 5 ;
+
+printf("Aluno:%s\n", aluno);
+printf("Matricula:%d\n", matricul);
+printf("Média:%.2f\n", media);
 
 }
