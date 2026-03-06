33302729a73f5cdc3efccf8dad1aba326f914059
airtnsnts <airtnsnts@gmail.com>
2026-03-06 11:45:21 -0300

    test

---
+++ Código C/conversão.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int a = 10;
+    int b = 3;
+    float quociente = (float) a / b; // Conversão explícita para float
+
+    printf("O quociente de %d e %d é: %.2f\n", a, b, quociente);
+    
+
+}
